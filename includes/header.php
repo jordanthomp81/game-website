fdbeaa0742ecd68d1cc77170ba503147063e5493
Jordan Thompson <jordanthomp81@gmail.com>
2018-05-21 09:32:32 -0500

    header work

--- includes/header.php
+++ includes/header.php
@@ -5,5 +5,38 @@
     <title>Homepage</title>
     <!-- Animate CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <!-- Application CSS Files -->
+    <link rel="stylesheet" href="src/css/base.css">
   </head>
   <body>
+    <div class="banner-small">We made a few changes! DaGames4uNow has become DaGamesNow.</div>
+    <div class="header-main-container">
+
+      <div class="header-inner-container">
+
+        <a href="#"><img id="main-logo" width="255px" height="38px" src="src/media/png/logo-2x.png" alt="site logo"></a>
+
+        <nav id="primary-nav-container">
+          <ul>
+            <li class="first-order current-menu-item"><a class="first-order-link" href="#">Home</a></li>
+            <li class="first-order dropdown"><a class="first-order-link" href="#">Categories</a>
+              <ul class="animated fadeIn">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Adventure</a></li>
+                <li><a href="#">Arcade</a></li>
+                <li><a href="#">Driving</a></li>
+                <li><a href="#">Fighting</a></li>
+                <li><a href="#">Shooting</a></li>
+                <li><a href="#">Puzzle & Skill</a></li>
+                <li><a href="#">Strategy</a></li>
+                <li><a href="#">Sports</a></li>
+              </ul>
+            </li>
+            <li class="first-order"><a class="first-order-link" href="#">New Releases</a></li>
+          </ul>
+        </nav>
+        <!-- <div class="clear"></div> -->
+
+      </div>
+
+    </div>
--- index.php
+++ index.php
@@ -1,3 +1,2 @@
 <?php include 'includes/header.php'; ?>
-  <?php echo 'Success'; ?>
 <?php include 'includes/footer.php'; ?>
